317bc6528ce30c0c746eb941a61039d41a341583
ubuntu-vm <guoshiwei@bogon.(none)>
2011-07-28 07:36:39 -0700

    -- Partly code on task_runner.

--- JavaLearning/HelloDate.java
+++ JavaLearning/HelloDate.java
@@ -1,8 +1,28 @@
 import java.util.*;
 
+/**
+ * @brief A desc
+ */
+class A{
+    public int a_int;
+    public char a_char;
+};
+
 public class HelloDate{
+
+    /**
+     * Main method
+     */
     public static void main(String[] args){
 	System.out.println("Hello, it's: ");
 	System.out.println(new Date());
+
+	A a = new A();
+
+	System.out.print("a.a_int == ");
+	System.out.println(a.a_int);
+
+	System.out.print("a.a_char == ");
+	System.out.println(a.a_char);
     }
 }
